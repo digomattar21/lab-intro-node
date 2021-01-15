2a8d403c0ba2cdb41b855191b47cba1579a6eba2
digomattar21 <digomattar@gmail.com>
2021-01-14 21:44:24 -0300

    Done com bonus

--- index.js
+++ index.js
@@ -1,17 +1,53 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+    
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a,b)=>a-b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (!this.items[pos]){
+      throw new Error('OutOfBounds');
+    } else {
+      return this.items[pos]
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.items.length){
+      return Math.max(...this.items)
+    } else throw new Error('EmptySortedList');
+  }
 
-  min() {}
+  min() {
+    if (this.items.length){
+      return Math.min(...this.items)
+    } else throw new Error('EmptySortedList');
+  }
+  
 
-  sum() {}
+  sum() {
+    if (this.items.length){
+      return this.items.reduce((acc,curr)=> acc+curr)
+    } else {
+      return 0
+    }
+  }
 
-  avg() {}
+  avg() {
+    if (this.items.length){
+      let avg =this.items.reduce((acc,curr)=> (acc+curr))
+      return avg/this.length
+
+    } else throw new Error('EmptySortedList')
+  }
+
 }
 
 module.exports = SortedList;
